e0865cc89ccb4c74251d46108f417d4c409609de
Neeraj Sharma <msbobby87@gmail.com>
2023-07-05 17:34:32 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,6 @@
 
 const express = require("express");
 const path = require("path");
-const threeSum = require("./script");
 const app = express();
 // Parse JSON bodies (as sent by API clients)
 app.use(express.json());
@@ -13,12 +12,6 @@ app.use(express.static(__dirname));
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname + "/main.html"));
-});
-
-app.post("/threesum", (req, res) => {
-  const { arr, target } = req.body;
-  const ans = threeSum(arr, target);
-  res.send({ message: ans });
 });
 
 module.exports = app;
